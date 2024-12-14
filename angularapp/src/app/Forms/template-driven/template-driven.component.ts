b43ffe33d098315ea7f12a5e0878b931b7066c3d
Kerim ARAT <m-kerimarat@hotmail.com>
2024-12-14 21:12:09 +0300

    Template Driven Forms 5.Kısım ( DropDown Menu Dynamic)

--- angularapp/src/app/Forms/template-driven/template-driven.component.ts
+++ angularapp/src/app/Forms/template-driven/template-driven.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { fromEvent } from 'rxjs';
@@ -5,17 +6,19 @@ import { fromEvent } from 'rxjs';
 @Component({
   selector: 'app-template-driven',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgFor],
   templateUrl: './template-driven.component.html',
   styleUrl: './template-driven.component.css'
 })
 export class TemplateDrivenComponent {
+  
   loginMode:loginModel;
+  countryMenu:countryDropMenu[]=[{text:"seciniz",value:0},{text:"Turkiye",value:1},{text:"Almanya",value:2},{text:"Fransa",value:3}]
   /* loginmodel2:loginModeC; */
   constructor(){
     this.loginMode= {userName:"",passworld:"",rememberME:false,country:0}
     /* class ilee örnek */
-    /* this.loginmodel2=new loginModeC(); */
+    /* this.loginmodel2=new  lo[ginModeC(); */
   }
   save(form:NgForm){
     console.log(form.value);
@@ -23,6 +26,13 @@ export class TemplateDrivenComponent {
   }
 
 }
+
+interface countryDropMenu{
+  text:string
+  value:number
+}
+
+
 interface loginModel{
   userName:string
   passworld:string
